0c24a06718c3b9d4f5dc7602f9a7de75cf8ae9f4
bilionaire777 <104227923+bilionaire777@users.noreply.github.com>
2022-05-18 11:02:56 +0200

    Update tic.py

--- tic.py
+++ tic.py
@@ -1,4 +1,5 @@
-from prettytable import PrettyTable
+
+
 print()
 #powitanie  
 print("Hello! Welcome in Tic Tac Toe Game. Before starting, please choose type of the game:")
@@ -39,6 +40,40 @@ board_ASCI = ('''
 3  .  |  .  |  .  
       |     |  ''')
 
+#tablica do grania
+board_0_work = [" ", "A", "|", "B", "|", "C"]
+board_1_work = ["1", "A1", "|", "B1", "|", "C1"]
+board_2_work = ["2", "A2", "|", "B2", "|", "C2"]
+board_3_work = ["3", "A3", "|", "B3", "|", "C3"]
+
+board_0 = [" ", "A", "|", "B", "|", "C"]
+board_1 = ["1", ".", "|", ".", "|", "."]
+board_2 = ["2", ".", "|", ".", "|", "."]
+board_3 = ["3", ".", "|", ".", "|", "."]
+
+
+
+
+
+
+#rozwiązania, które dają wygraną graczowi
+
+#board_1[1] == board_2[1] == board_3[1]
+
+#board_1[3] == board_2[3] == board_3[3]
+#board_1[5] == board_2[5] == board_3[5]
+
+#board_1[1] == board_1[3] == board_1[5]
+#board_2[1] == board_2[3] == board_2[5]
+#board_3[1] == board_3[3] == board_3[5]
+
+#board_1[1] == board_2[3] == board_3[5]
+#board_3[1] == board_2[3] == board_1[5]
+
+#oznakowanie miejsc w tablicy
+
+
+
 
 
 #tablica znaków do grania
@@ -67,16 +102,17 @@ if type_game == "2":
       print(f"{player_1} --> Player nr 1")
       print(f"{player_2} --> Player nr 2")
       
+      
       print()
       print(f"Ok {player_1}, now please choose what is your mark. For X type x like xerox and for O type o like open.")
-      player_1_mark = input("Your mark is: ")
+      player_1_mark = str(input("Your mark is: "))
       player_1_mark_up = str.upper(player_1_mark)
-      
+    
 
 
       while not player_1_mark_up in marks_table:
           print("Please provide correct mark: X or O (you can use lowercase too).")
-          player_1_mark = input("Your mark is: ")
+          player_1_mark = str(input("Your mark is: "))
           player_1_mark_up = str.upper(player_1_mark)
       else:
           if player_1_mark_up == "X":
@@ -89,8 +125,100 @@ if type_game == "2":
       print(f"{player_2} --> {player_2_mark_up}")
       print()
       
-      print(f"Ok guys, this is the board game. To start, {player_1} please type the cell address, where you want to sign your mark (for example please type: A2 or b2).")
-      print(board_ASCI)
+      print(f"Ok guys, this is the board game. Please type the cell address, where you want to sign your mark (for example please type: A2 or b2).")
+      print()
+      print(*board_0)
+      print(*board_1)
+      print(*board_2)
+      print(*board_3)
       print()
 
       
+
+
+
+
+      while not board_1_work[1] == board_2_work[1] == board_3_work[1] or board_1_work[3] == board_2_work[3] == board_3_work[3]:
+      
+          print(f"{player_1} your move!")
+          player_1_position = input("Please type your position: ")
+          player_1_position_upper = player_1_position.upper()
+          if player_1_position_upper == "A1" and board_1_work[1] == "A1":
+              board_1[1] = player_1_mark_up
+              board_1_work[1] = player_1_mark_up
+          elif player_1_position_upper == "A2" and board_2_work[1] == "A2":
+              board_2[1] = player_1_mark_up
+              board_2_work[1] = player_1_mark_up
+          elif player_1_position_upper == "A3" and board_3_work[1] == "A3":
+              board_3[1] = player_1_mark_up
+              board_3_work[1] = player_1_mark_up
+          elif player_1_position_upper == "B1" and board_1_work[3] == "B1":
+              board_1[3] = player_1_mark_up
+              board_1_work[3] = player_1_mark_up
+          elif player_1_position_upper == "B2" and board_2_work[3] == "B2":
+              board_2[3] = player_1_mark_up
+              board_2_work[3] = player_1_mark_up
+          elif player_1_position_upper == "B3" and board_3_work[3] == "B3":
+              board_3[3] = player_1_mark_up
+              board_3_work[3] = player_1_mark_up
+          elif player_1_position_upper == "C1" and board_1_work[5] == "C1":
+              board_1[5] = player_1_mark_up
+              board_1_work[5] = player_1_mark_up
+          elif player_1_position_upper == "C2" and board_2_work[5] == "C2":
+              board_2[5] = player_1_mark_up
+              board_2_work[5] = player_1_mark_up
+          elif player_1_position_upper == "C3" and board_3_work[5] == "C3":
+              board_3[5] = player_1_mark_up
+              board_3_work[5] = player_1_mark_up
+
+              
+              
+              
+              
+              
+              
+          print()
+          print(*board_0)
+          print(*board_1)
+          print(*board_2)
+          print(*board_3)
+          print()
+
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          #while player_1_position_upper in board_1 or board_2 or board_3:
+              #for i in board_1:
+                 # board_1[i] = player_1_mark_up
+             # for i in board_2:
+             #     board_2[i] = player_1_mark_up
+             # for i in board_3:
+             #     board_3[i] = player_1_mark_up
+             # print()
+             # print(*board_0)
+             # print(*board_1)
+             # print(*board_2)
+             # print(*board_3)
+             # print()
+         # else:
+          #    print("Please input valid position.")
+            
+
+
+          
+
+      
+      else:
+          print("You won.")
